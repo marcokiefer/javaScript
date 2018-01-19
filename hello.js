8bf15dcee6453d703b4d00de5297ee062d6aac3f
Marco Kiefer, Server <i2019kiefer@gbs-technikum.de, Server>
2018-01-19 08:35:47 +0100

    hello.js erstellt

---
+++ hello.js
@@ -0,0 +1,47 @@
+var zahl = 42;
+console.log(zahl);
+console.log(zahl/8);
+// Datentyp der Variablen
+console.log(typeof zahl);
+zahl = 42.12;
+console.log(zahl);
+console.log(typeof zahl);
+zahl = '42';
+console.log(zahl);
+console.log(typeof zahl);
+var dozent = 'Maier';
+var fach = '"Internetanwendungen"';
+var test = "\"Internetanwendungen\"";
+console.log(test);
+var ausgabe = fach + '\nAndres ' + dozent;
+console.log(ausgabe);
+console.log(12 / 5 + " Test");
+console.log(12 + 5 + " Test");
+console.log("Hallo " + 12 + 5);
+// Boolean
+var fertig = false;
+console.log(fertig);
+console.log(typeof fertig);
+console.log('---> false, 0 und leere Strings sind gleich');
+console.log('false == 0: '+(false == 0));
+console.log('false == "": '+(false == ""));
+console.log('0 == "": ' + (0 == ""));
+console.log('---> null und undefined sind untereinander gleich');
+console.log(null == undefined);
+console.log('---> NaN ist zu nichts gleich');
+console.log(NaN == NaN);
+console.log(42 == '42');
+console.log('---> Strikter Vergleich === zusätzlich Datentypvergleich');
+console.log(42 === '42');
+var i = 42;
+var j = '42';
+console.log(i == j);
+console.log(i === j );
+console.log('Ungleich: !=');
+console.log('striktes Ungleich: !==');
+console.log('---> undefined und null');
+
+var tmp = 42;
+console.log(tmp);
+console.log(typeof tmp);
+
